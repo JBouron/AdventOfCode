8c428f2174d1cd549cc54dbd88f56407d05fd6fe
Justinien Bouron <justinien.bouron@gmail.com>
2022-12-27 22:09:56 -0800

    Solution for day 22, part 2.

--- 22/solve.py
+++ 22/solve.py
@@ -38,15 +38,6 @@ class Board:
         self.grid = grid
         self.width = len(grid[0])
         self.height = len(grid)
-        #faceSideLen = 4
-        #self.faceId = {
-        #    [Position(2, 0)] = 1,
-        #    [Position(0, 1)] = 2,
-        #    [Position(1, 1)] = 3,
-        #    [Position(2, 1)] = 4,
-        #    [Position(2, 2)] = 5,
-        #    [Position(3, 2)] = 6,
-        #}
 
     # Compute the start index and the width of the row at height y.
     # The start index is the smallest x that is not out of bounds in this row.
@@ -63,7 +54,7 @@ class Board:
         return (r[0][0], len(r))
 
     # Compute the password when tracing `path` onto the board.
-    def computePassword(self, path):
+    def computePassword(self, path, part2):
         # Compute the starting position.
         startX = self.rowStartAndWidth(0)[0]
         currPos = Position(startX, 0)
@@ -101,27 +92,180 @@ class Board:
             else:
                 return nextPos, d
 
+        def faceId(p):
+            key = Position(p.x // 50, p.y // 50)
+            kToId = {
+                Position(2, 0): 1,
+                Position(1, 0): 2,
+                Position(1, 1): 3,
+                Position(1, 2): 4,
+                Position(0, 2): 5,
+                Position(0, 3): 6,
+            }
+            return kToId[key]
+
+        def getNextPosAndRotPart2(p, d):
+            # Split the logic between vertical and horizontal movements to make
+            # it easier.
+            fId = faceId(p)
+            x, y = p.x, p.y
+            nextX = None
+            nextY = None
+            nextDx = None
+            nextDy = None
+            # Right wrap.
+            if d.x == 1:
+                if fId == 1 and x == 149:
+                    nextX = 99
+                    nextY = 149 - y
+                    nextDx = -1
+                    nextDy = 0
+                elif fId == 3 and x == 99:
+                    nextX = 50 + y
+                    nextY = 49
+                    nextDx = 0
+                    nextDy = -1
+                elif fId == 4 and x == 99:
+                    nextX = 149
+                    nextY = 149 - y
+                    nextDx = -1
+                    nextDy = 0
+                elif fId == 6 and x == 49:
+                    nextX = y - 100
+                    nextY = 149
+                    nextDx = 0
+                    nextDy = -1
+            elif d.x == -1:
+                # Left wrap.
+                if fId == 2 and x == 50:
+                    nextX = 0
+                    nextY = 149 - y
+                    nextDx = 1
+                    nextDy = 0
+                if fId == 3 and x == 50:
+                    nextX = y - 50
+                    nextY = 100
+                    nextDx = 0
+                    nextDy = 1
+                if fId == 5 and x == 0:
+                    nextX = 50
+                    nextY = 149 - y
+                    nextDx = 1
+                    nextDy = 0
+                if fId == 6 and x == 0:
+                    nextX = y - 100
+                    nextY = 0
+                    nextDx = 0
+                    nextDy = 1
+            elif d.y == 1:
+                # down wrap
+                if fId == 1 and y == 49:
+                    nextX = 99
+                    nextY = x - 50
+                    nextDx = -1
+                    nextDy = 0
+                if fId == 4 and y == 149:
+                    nextX = 49
+                    nextY = x + 100
+                    nextDx = -1
+                    nextDy = 0
+                if fId == 6 and y == 199:
+                    nextX = 100 + x
+                    nextY = 0
+                    nextDx = 0
+                    nextDy = 1
+            elif d.y == -1:
+                # Up wrap
+                if fId == 1 and y == 0:
+                    nextX = x - 100
+                    nextY = 199
+                    nextDx = 0
+                    nextDy = -1
+                if fId == 2 and y == 0:
+                    nextX = 0
+                    nextY = x + 100
+                    nextDx = 1
+                    nextDy = 0
+                if fId == 5 and y == 100:
+                    nextX = 50
+                    nextY = 50 + x
+                    nextDx = 1
+                    nextDy = 0
+
+            if nextX is None:
+                assert nextY is None and nextDx is None and nextDy is None
+                nextDx = d.x
+                nextDy = d.y
+                # We are not going to another face.
+                if abs(d.x) == 1 and d.y == 0:
+                    # Horizontal movement.
+                    nextY = p.y
+                    s, w = self.rowStartAndWidth(p.y)
+                    nextX = s + (p.x - s + d.x) % w
+                else:
+                    # Vertical movement.
+                    assert d.x == 0 and abs(d.y) == 1
+                    nextX = p.x
+                    s, h = self.colStartAndHeight(p.x)
+                    nextY = s + (p.y - s + d.y) % h
+
+            nextPos = Position(nextX, nextY)
+            # Sanity check that we computed the wrap-around correctly.
+            assert self.grid[nextPos.y][nextPos.x] != Board.CellType.Oob
+            if self.grid[nextPos.y][nextPos.x] == Board.CellType.Rock:
+                # In case the next position lands on a rock, we don't move.
+                return p, d
+            else:
+                return nextPos, Position(nextDx, nextDy)
+
+        def rotateLeft(d):
+            res = {
+                Position(1, 0) : Position(0, -1),
+                Position(0, 1) : Position(1, 0),
+                Position(-1, 0) : Position(0, 1),
+                Position(0, -1) : Position(-1, 0),
+            }
+            return res[d]
+
+        def rotateRight(d):
+            res = {
+                Position(1, 0) : Position(0, 1),
+                Position(0, 1) : Position(-1, 0),
+                Position(-1, 0) : Position(0, -1),
+                Position(0, -1) : Position(1, 0),
+            }
+            return res[d]
+
         while len(moves) > 0:
             currMove = moves.pop(0)
             if "L" in currMove:
                 # Rotate to the left.
-                rotIdx = (rotIdx - 1) % len(DIRS)
-                currRot = DIRS[rotIdx]
+                currRot = rotateLeft(currRot)
             elif "R" in currMove:
                 # Rotate to the right.
-                rotIdx = (rotIdx + 1) % len(DIRS)
-                currRot = DIRS[rotIdx]
+                currRot = rotateRight(currRot)
             else:
                 # Make a few steps.
                 numSteps = int(currMove)
                 for i in range(numSteps):
-                    nextPos, currRot = getNextPosAndRot(currPos, currRot)
+                    if part2:
+                        nextPos, currRot = getNextPosAndRotPart2(currPos, currRot)
+                    else:
+                        nextPos, currRot = getNextPosAndRot(currPos, currRot)
                     if nextPos == currPos:
                         # Ran into a wall, stop the move and go to the next one.
                         break
                     else:
                         currPos = nextPos
-        return 1000 * (currPos.y + 1) + 4 * (currPos.x + 1) + rotIdx
+
+        rotVal = {
+            Position(1, 0) : 0,
+            Position(0, 1) : 1,
+            Position(-1, 0) : 2,
+            Position(0, -1) : 3,
+        }
+
+        return 1000 * (currPos.y + 1) + 4 * (currPos.x + 1) + rotVal[currRot]
 
 def parseInput(inputFile):
     fd = open(inputFile, "r")
@@ -153,10 +297,11 @@ def parseInput(inputFile):
 
 def part1(inputFile):
     board, path = parseInput(inputFile)
-    return board.computePassword(path)
+    return board.computePassword(path, False)
 
 def part2(inputFile):
-    return 0
+    board, path = parseInput(inputFile)
+    return board.computePassword(path, True)
 
 if __name__ == "__main__":
     inputFile = sys.argv[1]
