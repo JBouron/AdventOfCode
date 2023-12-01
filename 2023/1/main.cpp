e7e314bbfd961bc5aca5eef7258d65b26f00b315
Justinien Bouron <justinien.bouron@gmail.com>
2023-11-30 23:06:49 -0800

    Improve solution for 2023 - Day 1.

--- 2023/1/main.cpp
+++ 2023/1/main.cpp
@@ -2,73 +2,85 @@
 #include <fstream>
 #include <algorithm>
 
+static const std::string NUMBERS[] = {
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+};
+
+// Check if a char is a digit.
+// @param c: The char to test.
+// @return: true if the char is a digit, false otherwise.
+static bool isDigit(char const c) {
+    return '0' <= c && c <= '9';
+};
+
+// Get the first digit or first spelled digit from a string.
+// @param str: The string to extract the first digit/number from.
+// @param incSpelled: If true, include spelled out numbers (part 2 only)
+// otherwise only look at digits.
+// @return: The first digit in the string.
+static int firstDigitFromString(std::string const str, bool const incSpelled) {
+    auto const firstDigit(std::find_if(str.cbegin(), str.cend(), isDigit));
+    if (!incSpelled) {
+        return *firstDigit - '0';
+    } else {
+        std::string::const_iterator firstNum(str.cend());
+        int firstNumIdx(-1);
+        for (int i(0); i < 9; ++i) {
+            auto const num(NUMBERS[i]);
+            auto const p(std::search(str.cbegin(), str.cend(),
+                                     num.cbegin(), num.cend()));
+            if (p < firstNum) {
+                firstNum = p;
+                firstNumIdx = i;
+            }
+        }
+        if (firstDigit < firstNum) {
+            return *firstDigit - '0';
+        } else {
+            return firstNumIdx + 1;
+        }
+    }
+}
+
+// Get the last digit or last spelled digit from a string.
+// @param str: The string to extract the last digit/number from.
+// @param incSpelled: If true, include spelled out numbers (part 2 only)
+// otherwise only look at digits.
+// @return: The last digit in the string.
+static int lastDigitFromString(std::string const str, bool const incSpelled) {
+    auto const lastDigit(std::find_if(str.crbegin(), str.crend(), isDigit));
+    if (!incSpelled) {
+        return *lastDigit - '0';
+    } else {
+        std::string::const_reverse_iterator lastNum(str.crend());
+        int lastNumIdx(-1);
+        for (int i(0); i < 9; ++i) {
+            auto const num(NUMBERS[i]);
+            auto const p(std::search(str.crbegin(), str.crend(),
+                                     num.crbegin(), num.crend()));
+            if (p < lastNum) {
+                lastNum = p;
+                lastNumIdx = i;
+            }
+        }
+        if (lastDigit < lastNum) {
+            return *lastDigit - '0';
+        } else {
+            return lastNumIdx + 1;
+        }
+    }
+}
+
 int main(int const argc, char const * const * const argv) {
     std::ifstream input(argv[1], std::ios::in);
     std::string line;
     int part1(0);
     int part2(0);
-    std::string const numbers[] = {
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    };
     while (std::getline(input, line)) {
-        auto const isDigit([](char const c) {
-            return '0' <= c && c <= '9';
-        });
-
-        // Part 1.
-        // Find the first and last digit. All lines have at least one digit.
-        auto const firstDigit(std::find_if(line.cbegin(),
-                                           line.cend(),
-                                           isDigit));
-        auto const lastDigit(std::find_if(line.crbegin(),
-                                          line.crend(),
-                                          isDigit));
-        part1 += int(*firstDigit - '0') * 10;
-        part1 += int(*lastDigit - '0');
-
-        // Part 2.
-        // Find the first and last number. A number is either a digit or a
-        // spelled out number in the string. All lines have at least one number.
-        // Re-use firstDigit and lastDigit from part1.
-        std::string::const_iterator firstSpelled(line.cend());
-        int firstSpelledIdx(-1);
-        std::string::const_reverse_iterator lastSpelled(line.crend());
-        int lastSpelledIdx(-1);
-        for (int i(0); i < 9; ++i) {
-            auto const num(numbers[i]);
-            auto const f(std::search(line.cbegin(),
-                                     line.cend(),
-                                     num.cbegin(),
-                                     num.cend()));
-            if (f < firstSpelled) {
-                firstSpelled = f;
-                firstSpelledIdx = i;
-            }
-            auto const l(std::search(line.crbegin(),
-                                     line.crend(),
-                                     num.crbegin(),
-                                     num.crend()));
-            if (l != line.crend() && l < lastSpelled) {
-                lastSpelled = l;
-                lastSpelledIdx = i;
-            }
-        }
-
-        auto const f(std::min(firstDigit, firstSpelled));
-        if (firstSpelledIdx == -1 || isDigit(*f)) {
-            // First digit is before the first spelled out number.
-            part2 += int(*firstDigit - '0') * 10;
-        } else {
-            // First spelled out number is before the first digit.
-            part2 += (firstSpelledIdx + 1) * 10;
-        }
-
-        auto const l(std::min(lastDigit, lastSpelled));
-        if (lastSpelledIdx == -1 || isDigit(*l)) {
-            part2 += int(*lastDigit - '0');
-        } else {
-            part2 += (lastSpelledIdx + 1);
-        }
+        part1 += firstDigitFromString(line, false) * 10
+                 + lastDigitFromString(line, false);
+        part2 += firstDigitFromString(line, true) * 10
+                 + lastDigitFromString(line, true);
     }
     std::cout << "Part 1: " << part1 << std::endl;
     std::cout << "Part 2: " << part2 << std::endl;
